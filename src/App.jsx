9fbe5e5156e576b1e9b22ff1515ef211e34b420f
Frederico27 <fredericomarcal27@gmail.com>
2025-05-11 21:56:36 +0900

    el classico

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Card from "./Card";
 import Footer from "./Footer";
-import Modal from "./Modal";
 
 function App() {
  
