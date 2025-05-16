227de4b293ae2ecc47ee03c54f06d34b8593c854
Frederico27 <fredericomarcal27@gmail.com>
2025-05-17 00:53:41 +0900

    epl saturday w36

--- src/App.jsx
+++ src/App.jsx
@@ -54,35 +54,36 @@ function App() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 mt-3">
         {/* Card goes Here */}
 
+
         <Card
-          title="Espanyol vs Barcelona"
-          loron={"16 Maiu 2025"}
-          oras={"Kick off 04:30 OTL | Komentariu Ingles"}
+          title="Aston Villa vs Tottenham Hotspur"
+          loron={"17 Maiu 2025"}
+          oras={"Kick off 03:30 OTL | Komentariu Espanha"}
           tumbnail={
-            "https://i.ytimg.com/vi/ainwTsAFO78/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDwwDVpfNMua2U1_7MO_wk9t8Ymkw"
+            "https://static.standard.co.uk/2021/08/10/09/PLAstonVillaTottenham2122.jpg?crop=8:5,smart&quality=75&auto=webp&width=1000"
           }
-          videoSrc="https://a166aivottlinear-a.akamaihd.net/OTTB/dub-nitro/live/clients/dash/enc/k0duzgfejg/out/v1/70a50b1bda944628b8e7e66ab4069419/cenc.mpd"
+          videoSrc="https://a151aivottlinear-a.akamaihd.net/OTTB/pdx-nitro/live/clients/dash/enc/xnk4m9bnxt/out/v1/4ced7b7329a54652b9bb0521ed38bd4d/cenc.mpd"
           drmLicenseUrl="https://real-madrid-drm-url"
           nsurl={
-            "intent://a166aivottlinear-a.akamaihd.net/OTTB/dub-nitro/live/clients/dash/enc/k0duzgfejg/out/v1/70a50b1bda944628b8e7e66ab4069419/cenc.mpd|User-Agent=Mozilla/5.0 (Web0S; Linux/SmartTV) AppleCoreMedia/537.36 iPhone (KHTML, like Gecko) Version/4.0 Chromecast/107.0.5304.91 Mobile Safari/537.36&drmScheme=clearkey&drmLicense=620e51b82596475517a27aa425c52280:2b9ba811e9c5aeafc8ae1b71cdca4d6a#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=LIVETV;end"
+            "intent://a151aivottlinear-a.akamaihd.net/OTTB/pdx-nitro/live/clients/dash/enc/xnk4m9bnxt/out/v1/4ced7b7329a54652b9bb0521ed38bd4d/cenc.mpd|User-Agent=Mozilla/5.0 (Web0S; Linux/SmartTV) AppleCoreMedia/537.36 iPhone (KHTML, like Gecko) Version/4.0 Chromecast/107.0.5304.91 Mobile Safari/537.36&drmScheme=clearkey&drmLicense=0eab5a3f3e3b4ba5d42d40ca30d17571:f3f061ded9b70e8160590d5802ecda6d#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=LIVETV;end"
           }
         />
 
+
         <Card
-          title="Espanyol vs Barcelona"
-          loron={"16 Maiu 2025"}
-          oras={"Kick off 04:30 OTL | Komentariu Ingles"}
+          title="Chelsea vs Manchester United"
+          loron={"17 Maiu 2025"}
+          oras={"Kick off 04:15 OTL | Komentariu Espanha"}
           tumbnail={
-            "https://i.ytimg.com/vi/ainwTsAFO78/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDwwDVpfNMua2U1_7MO_wk9t8Ymkw"
+            "https://static.standard.co.uk/2023/08/10/14/PLChelseaManUtd23v1.jpg?crop=8:5,smart&quality=75&auto=webp&width=1000"
           }
-          vide
-          videoSrc="https://otte.live.fly.ww.aiv-cdn.net/dub-nitro/live/clients/dash/enc/6fbnr3ei4b/out/v1/57d2ae96a4cc4230881cd801b389edf6/cenc.mpd"
+          videoSrc="https://a151aivottlinear-a.akamaihd.net/OTTB/pdx-nitro/live/clients/dash/enc/bmnelo5c7a/out/v1/3ce2cdc4589f46189322bd3717c77957/cenc.mpd"
           drmLicenseUrl="https://real-madrid-drm-url"
           nsurl={
-            "intent://otte.live.fly.ww.aiv-cdn.net/dub-nitro/live/clients/dash/enc/6fbnr3ei4b/out/v1/57d2ae96a4cc4230881cd801b389edf6/cenc.mpd%7CUser-Agent=Mozilla/5.0&drmScheme=clearkey&drmLicense=9afd72f20573001c23672d2158892a5f:9bc32df48a2efac30072b7e5c683bcd1#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=LIVETV;end"
+            "intent://a151aivottlinear-a.akamaihd.net/OTTB/pdx-nitro/live/clients/dash/enc/bmnelo5c7a/out/v1/3ce2cdc4589f46189322bd3717c77957/cenc.mpd|User-Agent=Mozilla/5.0 (Web0S; Linux/SmartTV) AppleCoreMedia/537.36 iPhone (KHTML, like Gecko) Version/4.0 Chromecast/107.0.5304.91 Mobile Safari/537.36&drmScheme=clearkey&drmLicense=44dd9cd370b08a868ead115fe84ecfde:bff19ab0a51cf14e848389b152913fd0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=LIVETV;end"
           }
         />
-
+        
         <Card
           title="Live Liga Inglesa"
           oras={"Kanal Foka ba Jogu sira seluk Liga Inglesa nian"}
